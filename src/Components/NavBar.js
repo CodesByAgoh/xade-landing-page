b6eb0801ff8af5c575148a710b6cdc31e3cf0d80
KELLY <kelly@KELLYs-MacBook-Pro.local>
2022-06-06 17:54:03 +0100

    Menu icon, now working

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,30 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import 'Stylesheets/NavBar.css'
 import {Link} from 'react-scroll'
-/* import './Responsive.css' */
+import '../Stylesheets/Responsive.css'
 
 
 
+/*  const toggleBotton = document.querySelector('.toggle__botton');
+const  navbarLinks = document.querySelector('.right__side');
 
-/* const toggleBotton = document.getElementsByClassName('toggle__botton')[0]
-const navbarLinks = document.getElementsByClassName('right__side')[0]
+ toggleBotton.addEventListener( 'click',() => {
+  toggleBotton.classList.toggle('active');
+  navbarLinks.classList.toggle('active');
+})  
+ */
 
- toggleBotton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})  */
 
-
+ 
 function NavBar() {
+
+  const [showMenu, setShowMenu] = useState(false)
+
+let menu
+
+  if (showMenu) {
+    menu =  <div className='right__side3 right__side'>
+    <Link className='right__side__text' to="section2" spy={true} smooth={true} offset={50} duration={500} >
+       Personal
+       </Link>
+           
+       <Link className='right__side__text' to="section3b" spy={true} smooth={true} offset={50} duration={500} >
+       Business
+       </Link>
+       <Link className='right__side__text' to="section4" spy={true} smooth={true} offset={50} duration={500} >
+       Contact Us
+       </Link>
+           <a className='webapp' href='https://app.xade.finance/'> Web App</a>
+      </div> 
+  }
+
   return (
     <div className='navbar'>
         <div className='left__side'>
             <h1 className='left__side__text'>xade</h1>
         </div>
-        <a href='#' className='toggle__botton'>
+        <div className='toggle__botton' onClick={() => setShowMenu(!showMenu)}>
           <span className='bar'></span>
           <span className='bar'></span>
           <span className='bar'></span>
-        </a>
+          {menu}
+        </div>
+
         <div className='right__side'>
          <div className='right__side2'>
          <Link className='right__side__text' to="section2" spy={true} smooth={true} offset={50} duration={500} >
@@ -42,6 +67,7 @@ function NavBar() {
             
         </div>
     </div>
+    
   )
 }
 
